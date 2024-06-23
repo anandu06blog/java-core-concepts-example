ec8f8a62b7cd352accc38a248b399329e4b40b55
anandu06blog <113542195+anandu06blog@users.noreply.github.com>
2024-06-23 18:50:51 +0530

    Java Core concepts

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    /**
+     * First Hello World Program
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
